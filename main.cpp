787120894a64002a91065840c57184e25d7c09be
joaquinbian <joacobian21@gmail.com>
2024-09-29 02:29:38 -0300

    actividad 1 de ejercicio de archivos

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cstring>
+#include "Pais.h"
+
+bool paisExiste(char codigoPais[4]);
+int getCantidadPaises();
+void mostrarPaises();
+
+int main(){
+
+    char codigo[4];
+    bool exists;
+
+    std::cout << "Ingrese el codigo del pais: ";
+    std::cin >> codigo;
+
+    exists =  paisExiste(codigo);
+
+    if(exists){
+        std::cout << "El pais existe " << std::endl;
+    } else {
+        std::cout << "El pais no existe " << std::endl;
+
+    }
+
+    std::cout << "La cantidad de paises que hay es " << getCantidadPaises() << std::endl;
+    return 0;
+}
+
+int getCantidadPaises(){
+    FILE *pFile;
+    Pais pais;
+    int cantidad;
+
+
+    //todo esto es de la clase 
+    //abre el archivo en modo lectura
+    pFile = fopen("paises.dat", "rb");
+
+    if(pFile == nullptr){
+        return -1;
+    }
+
+    //movemos el cursor al final del archivo
+    fseek(pFile, 0, SEEK_END);
+
+
+    //ftell nos dice la cantidad de bytes que hay desde el inicio hasta
+    //donde esta el cursor(que lo movimos al final), entonces
+    //nos dice la cantidad de bytes del archivo, y lo dividimos por
+    //lo que ocupa un solo registro, obteniendo el total
+    cantidad = ftell(pFile) / sizeof(Pais);
+
+    if(cantidad >= 1){
+        return cantidad;
+    }
+
+    fclose(pFile);
+
+    return 0;
+};
+
+bool paisExiste(char codigoPais[4]){
+    FILE * pFile;
+    Pais pais;
+    int totalPaises = getCantidadPaises();
+
+    pFile = fopen("paises.dat", "rb");
+
+    for(int i = 0; i < totalPaises; i++){
+        fread(&pais, sizeof(Pais), 1, pFile);
+        //compara 2 chars, si son iguales devuelve 0
+        if(strcmp(pais.getCodigo(), codigoPais) == 0){
+            return true;
+        }
+
+    }
+
+
+    return false;
+};
