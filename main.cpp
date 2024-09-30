6fd18cbd9840e0eb285d923765446184b7be976d
joaquinbian <joacobian21@gmail.com>
2024-09-29 23:15:10 -0300

    actvidad 2(faltan validaciones)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <cstring>
 #include "Pais.h"
+#include "Ciudad.h"
+
 
 bool paisExiste(char codigoPais[4]);
+bool existeCiudad(int idCiudad);
 int getCantidadPaises();
 void listarPaises();
+void agregarPais();
+void cargarPais(Pais &pais);
 
 int main(){
 
@@ -26,6 +31,9 @@ int main(){
     } */
     std::cout << "La cantidad de paises que hay es " << getCantidadPaises() << std::endl;
     //actividad 3
+    //listarPaises();
+    //actividad 2
+    agregarPais();
     listarPaises();
     
     return 0;
@@ -107,4 +115,102 @@ void listarPaises(){
     }
 
     fclose(pFile);
+};
+
+void agregarPais(){
+    FILE *pFile;
+    Pais registro;
+
+    pFile = fopen("paises.dat", "ab");
+
+    if(pFile == nullptr){
+        std::cout << "NO se pudo abrir el archivo" << std::endl;
+        exit(-1);
+    }
+
+    cargarPais(registro);
+
+    int res = fwrite(&registro, sizeof(Pais), 1, pFile);
+
+    if(res == 1){
+        std::cout<< "El pais se ha guardado con exito" << std::endl;
+    } else {
+        std::cout<<"no se ha podido guardar el pais" << std::endl;
+    }
+
+    
+};
+
+
+bool existeCiudad(int idCiudad){
+    FILE *pFile;
+    Ciudad registro;
+
+    pFile = fopen("ciudades.dat", "rb");
+
+    if(pFile == nullptr){
+        std::cout << "no se pudo abrir el archivo ciudad" << std::endl;
+        exit(-1);
+    }
+    
+   while(fread(&registro, sizeof(Ciudad), 1, pFile) == 1){
+        if(registro.getID() == idCiudad){
+            fclose(pFile);
+            return true;
+        }
+   }
+
+    fclose(pFile);
+   return false;
+};
+
+//enviamos una referencia pq sino crea una copia de un archivo
+void cargarPais(Pais &pais){
+    std::string codigo = "";
+    std::string codigo2 = "";
+    std::string nombre= "";
+    std::string continente = "";
+    float superficie = 0.0;
+    int poblacion = 0;
+    short independencia = 0;
+    float expectativaDeVida = 0.0;
+    int capital = 0;
+    std::cout << "Ingrese el codigo de pais: ";
+    std::cin >> codigo;
+    std::cout << std::endl;
+    std::cout << "Ingrese el codigo2 de pais: ";
+    std::cin >> codigo2;
+    std::cout << std::endl;
+    std::cout << "Ingrese el nombre de pais: ";
+    std::cin >> nombre;
+    std::cout << std::endl;
+    std::cout << "Ingrese el continente de pais: ";
+    std::cin >> continente;
+    std::cout << std::endl;
+    std::cout << "Ingrese el superficie de pais: ";
+    std::cin >> superficie;
+    std::cout << std::endl;
+    std::cout << "Ingrese el poblacion de pais: ";
+    std::cin >> poblacion;
+    std::cout << std::endl;
+    std::cout << "Ingrese el independencia de pais: ";
+    std::cin >> independencia;
+    std::cout << std::endl;
+    std::cout << "Ingrese el expectativa De Vida de pais: ";
+    std::cin >> expectativaDeVida;
+    do{
+        std::cout << std::endl;
+        std::cout << "Ingrese el capital de pais: ";
+        std::cin >> capital;
+    }while(!existeCiudad(capital));
+
+    pais.setCodigo(codigo.c_str());
+    pais.setCodigo2(codigo2.c_str());
+    pais.setNombre(nombre.c_str());
+    pais.setContinente(continente.c_str());
+    pais.setSuperficie(superficie);
+    pais.setPoblacion(poblacion);
+    pais.setIndependencia(independencia);
+    pais.setExpectativaDeVida(expectativaDeVida);
+    pais.setIDCapital(capital);
 };
